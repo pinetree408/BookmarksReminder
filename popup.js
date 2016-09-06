b7af7e8203cdc8e48d0bc0a78125f48f6de55cfa
leesangyoon <pinetree408@gmail.com>
2016-09-06 15:13:45 +0900

    Add function which uses chrome alarm api to test

--- popup.js
+++ popup.js
@@ -59,6 +59,27 @@ function dumpNode(bookmarkNode, query) {
   return li;
 }
 
+var alarmClock = {
+
+        onHandler : function(e) {
+            chrome.alarms.create("myAlarm", {delayInMinutes: 0.1, periodInMinutes: 0.2} );
+                    window.close();
+        },
+
+        offHandler : function(e) {
+            chrome.alarms.clear("myAlarm");
+                    window.close();
+        },
+
+        setup: function() {
+            var a = document.getElementById('alarmOn');
+            a.addEventListener('click',  alarmClock.onHandler );
+            var a = document.getElementById('alarmOff');
+            a.addEventListener('click',  alarmClock.offHandler );
+        }
+};
+
 document.addEventListener('DOMContentLoaded', function () {
   dumpBookmarks();
+  alarmClock.setup();
 });
