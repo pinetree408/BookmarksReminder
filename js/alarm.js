d304c73d9a49746d42e0afea518a468645f3d6b9
leesangyoon <pinetree408@gmail.com>
2017-01-15 03:03:20 +0900

    Add logic which uses local storage for random generate index & Add module which generate bookmark list dump

--- js/alarm.js
+++ js/alarm.js
@@ -1,33 +1,43 @@
 (function () {
   'use strict';
 
-  var alarmName = 'remindme';
-
   function checkAlarm(callback) {
-    chrome.alarms.getAll(function(alarms) {
-      var hasAlarm = alarms.some(function(a) {
-        return a.name == alarmName;
+    chrome.storage.local.get('alarmName', function(result) {
+      chrome.alarms.getAll(function(alarms) {
+        var hasAlarm = alarms.some(function(a) {
+          return a.name == result['alarmName'];
+        });
+        var newLabel;
+        if (hasAlarm) {
+          newLabel = 'Cancel alarm';
+        } else {
+          newLabel = 'Activate alarm';
+        }
+        document.getElementById('toggleAlarm').innerText = newLabel;
+        if (callback) {
+          callback(hasAlarm);
+        }
       });
-      var newLabel;
-      if (hasAlarm) {
-        newLabel = 'Cancel alarm';
-      } else {
-        newLabel = 'Activate alarm';
-      }
-      document.getElementById('toggleAlarm').innerText = newLabel;
-      if (callback) {
-        callback(hasAlarm);
-      }
-    })
+    });
   }
 
   function createAlarm() {
-    chrome.alarms.create(alarmName, {
-      delayInMinutes: 0.1, periodInMinutes: 0.1});
+    dumpBookmarks(function(result){
+      var randomIndex = Math.floor(Math.random() * result.length);
+      var alarmName = result[randomIndex]['title'];
+      chrome.alarms.create(alarmName, {
+        delayInMinutes: 0.1, periodInMinutes: 0.3});
+
+      var obj = {};
+      obj['alarmName'] = alarmName
+      chrome.storage.local.set(obj);
+    });
   }
 
   function cancelAlarm() {
-    chrome.alarms.clear(alarmName);
+    chrome.storage.local.get('alarmName',function(result) { 
+      chrome.alarms.clear(result['alarmName']);
+    });
   }
 
   function doToggleAlarm() {
---
+++ js/bookmarks.js
@@ -0,0 +1,25 @@
+function dumpBookmarks(callBack) {
+  var bookmarkTreeNodes = chrome.bookmarks.getTree(
+    function(bookmarkTreeNodes) {
+      var list = [];
+      dumpTreeNodes(bookmarkTreeNodes, list);
+      callBack(list);
+    });
+}
+function dumpTreeNodes(bookmarkNodes, list) {
+  var i;
+  for (i = 0; i < bookmarkNodes.length; i++) {
+    dumpNode(bookmarkNodes[i], list);
+  }
+}
+function dumpNode(bookmarkNode, list) {
+  if (!bookmarkNode.children) {
+    var bookmarkObject = {};
+    bookmarkObject['url'] = bookmarkNode.url;
+    bookmarkObject['title'] = bookmarkNode.title;
+    list.push(bookmarkObject);
+  }
+  if (bookmarkNode.children && bookmarkNode.children.length > 0) {
+    dumpTreeNodes(bookmarkNode.children, list);
+  }
+}
