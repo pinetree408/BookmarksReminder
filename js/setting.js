01f8762ebd5fa987ee1430a0c5f61ee8270dbe89
Sangyoon Lee <pinetree408@gmail.com>
2017-01-26 14:13:10 +0900

    Add function which shows current setting

--- js/setting.js
+++ js/setting.js
@@ -4,4 +4,11 @@ function addSetting() {
   obj["setting"] = setting
   chrome.storage.local.set(obj);
 }
+chrome.storage.local.get("setting", function(result) {
+  var setting = 60;
+  if (result["setting"]) {
+    setting = result["setting"];
+  }
+  document.getElementById("setting-value").value = setting; 
+});
 document.getElementById("setting").addEventListener("click", addSetting);
