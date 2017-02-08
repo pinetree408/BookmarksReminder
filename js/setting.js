1d5d3a7b927799eba71b02c9b96bf40972c2c342
leesangyoon <pinetree408@gmail.com>
2017-02-09 01:10:16 +0900

    Add logic which checks validate of minutes input

--- js/setting.js
+++ js/setting.js
@@ -6,6 +6,13 @@ function addSetting() {
     chrome.storage.local.set(obj);
     document.getElementById("setting-value-view").innerHTML = setting;
   }
+  if ((setting == "") || (Number(setting) < 15) || (Number(setting) > 120)) {
+    document.getElementById("setting-input").classList.add("is-invalid");
+  }
+  if (document.getElementById("setting-input").classList.contains("is-invalid") == false) {
+    document.getElementById("setting-value").value = "";
+    document.getElementById("setting-input").classList.remove("is-dirty");
+  }
 }
 chrome.storage.local.get("setting", function(result) {
   var setting = 60;
