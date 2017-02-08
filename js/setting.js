6522853106d48018e91f4ba4b91e9c2d82c7b8ee
Sangyoon Lee <pinetree408@gmail.com>
2017-02-08 18:40:28 +0900

    Add realtime setting value updating function

--- js/setting.js
+++ js/setting.js
@@ -3,6 +3,7 @@ function addSetting() {
   var obj = {};
   obj["setting"] = setting
   chrome.storage.local.set(obj);
+  document.getElementById("setting-value-view").innerHTML = setting;
 }
 chrome.storage.local.get("setting", function(result) {
   var setting = 60;
